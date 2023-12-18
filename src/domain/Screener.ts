881e2841492a5ecd38238fb6733f72e7af88fecf
Charles Feval <fevalc>
2023-12-18 10:43:00 -0800

    Improve code structure and readability in Screener class

--- src/domain/Screener.ts
+++ src/domain/Screener.ts
@@ -22,11 +22,63 @@ interface ScreeningGuidelineChange {
 }
 
 export class Screener {
-  constructor(private deps: IScreenerDeps) {
+  constructor(private deps: IScreenerDeps) {}
 
+  ScreenMailAsync = async (): Promise<void> => {
+    await this.deps.mailbox.connectAsync()
+    try {
+      const mails: IDictionary<IMail[]> = await this.fetchAllMailsAsync();
+      await this.determineGuidelineChanges(mails);
+      await this.moveMails(mails);
+    } catch (error) {
+      this.deps.log.error(error)
+    }
+    await this.deps.mailbox.disconnectAsync()
   }
 
-  private screenFolder = async (mails: IMail[], correspondingScreeningResult: ScreeningResult): Promise<ScreeningGuidelineChange[]> => {
+  private async fetchAllMailsAsync() {
+    const mails: IDictionary<IMail[]> = {};
+    for (let i = 0; i < this.deps.folders.aliases.length; i++) {
+      const alias = this.deps.folders.aliases[i];
+      this.deps.log.debug(`fetchAllMailsAsync: ${alias} - started`);
+      const folder = this.deps.folders.folders[alias];
+      mails[folder.folder] = await this.deps.mailbox.getMailAsync(folder.folder);
+      this.deps.log.debug(`fetchAllMailsAsync: ${alias} - fetched ${mails[folder.folder].length} emails`);
+      if (folder.screeningFolder !== folder.folder) {
+        this.deps.log.debug(`fetchAllMailsAsync: ${alias} - fetching screening folder (${folder.screeningFolder})`);
+        mails[folder.screeningFolder] = await this.deps.mailbox.getMailAsync(folder.screeningFolder);
+        this.deps.log.debug(`fetchAllMailsAsync: ${alias} - fetched ${mails[folder.screeningFolder].length} in screening folder`);
+      }
+      this.deps.log.debug(`fetchAllMailsAsync: ${alias} - done`);
+    }
+    return mails;
+  }
+
+
+  /**
+   * Determines if there are any changes to the screening guidelines
+   * @param mails 
+   */
+  private async determineGuidelineChanges(mails: IDictionary<IMail[]>) {
+    let changes = [] as ScreeningGuidelineChange[];
+
+    for (let i = 0; i < this.deps.folders.aliases.length; i++) {
+      const alias = this.deps.folders.aliases[i];
+      this.deps.log.debug(`determineChanges: ${alias} - started`)
+      // We don't learn what goes into "for screening": it's a default behavior
+      // for senders that are not yet known.
+      if (alias !== FOR_SCREENING_FOLDER_ALIAS) {
+        const screeningFolder = this.deps.folders.folders[alias].screeningFolder
+        changes = changes.concat(await this.screenFolder(mails[screeningFolder],
+          { result: ScreeningResultType.TargetFolder, targetFolderAlias: alias }));
+      }
+      this.deps.log.debug(`determineChanges: ${alias} - done`)
+    }
+
+    await this.applyGuidelineChanges(changes);
+  }
+
+  private async screenFolder(mails: IMail[], correspondingScreeningResult: ScreeningResult): Promise<ScreeningGuidelineChange[]> {
     const changes: ScreeningGuidelineChange[] = []
     await Promise.all(mails.map(async mail => {
       const screeningResult = await this.deps.senderScreeningProvider.getScreeningResultAsync(mail.sender)
@@ -41,12 +93,13 @@ export class Screener {
     return changes
   }
 
-  private getFolderForScreeningResult = (screeningResult: ScreeningResult): Folder => {
-    if (screeningResult.result === ScreeningResultType.RequiresManualScreening) {
-      return this.deps.folders.folders[FOR_SCREENING_FOLDER_ALIAS].folder
-    } else {
-      return this.deps.folders.folders[screeningResult.targetFolderAlias as string].folder
-    }
+  private async applyGuidelineChanges(changes: ScreeningGuidelineChange[]) {
+    await Promise.all(changes.map(async change =>
+      {
+        this.deps.log.debug(`applyGuidelineChanges: Applying guideline change for ${change.sender}: ${change.newGuideline}`)
+        await this.deps.senderScreeningProvider.addScreeningGuidelineAsync(change.sender, change.newGuideline)
+      }
+    ))
   }
 
   private async moveMails(mails: IDictionary<IMail[]>) {
@@ -77,65 +130,11 @@ export class Screener {
     this.deps.log.debug(`moveMailsInFolderAsync: ${folder} - done screening ${mails.length} mails`)
   }
 
-  /**
-   * Determines if there are any changes to the screening guidelines
-   * @param mails 
-   */
-  private async determineGuidelineChanges(mails: IDictionary<IMail[]>) {
-    let changes = [] as ScreeningGuidelineChange[];
-
-    for (let i = 0; i < this.deps.folders.aliases.length; i++) {
-      const alias = this.deps.folders.aliases[i];
-      this.deps.log.debug(`determineChanges: ${alias} - started`)
-      // We don't learn what goes into "for screening": it's a default behavior
-      // for senders that are not yet known.
-      if (alias !== FOR_SCREENING_FOLDER_ALIAS) {
-        const screeningFolder = this.deps.folders.folders[alias].screeningFolder
-        changes = changes.concat(await this.screenFolder(mails[screeningFolder],
-          { result: ScreeningResultType.TargetFolder, targetFolderAlias: alias }));
-      }
-      this.deps.log.debug(`determineChanges: ${alias} - done`)
+  private getFolderForScreeningResult(screeningResult: ScreeningResult): Folder {
+    if (screeningResult.result === ScreeningResultType.RequiresManualScreening) {
+      return this.deps.folders.folders[FOR_SCREENING_FOLDER_ALIAS].folder
+    } else {
+      return this.deps.folders.folders[screeningResult.targetFolderAlias as string].folder
     }
-
-    await this.applyGuidelineChanges(changes);
-  }
-
-  private applyGuidelineChanges = async (changes: ScreeningGuidelineChange[]) => {
-    await Promise.all(changes.map(async change =>
-      {
-        this.deps.log.debug(`applyGuidelineChanges: Applying guideline change for ${change.sender}: ${change.newGuideline}`)
-        await this.deps.senderScreeningProvider.addScreeningGuidelineAsync(change.sender, change.newGuideline)
-      }
-    ))
-  }
-
-  ScreenMailAsync = async (): Promise<void> => {
-    await this.deps.mailbox.connectAsync()
-    try {
-      const mails: IDictionary<IMail[]> = await this.fetchAllMailsAsync();
-      await this.determineGuidelineChanges(mails);
-      await this.moveMails(mails);
-    } catch (error) {
-      this.deps.log.error(error)
-    }
-    await this.deps.mailbox.disconnectAsync()
-  }
-
-  private async fetchAllMailsAsync() {
-    const mails: IDictionary<IMail[]> = {};
-    for (let i = 0; i < this.deps.folders.aliases.length; i++) {
-      const alias = this.deps.folders.aliases[i];
-      this.deps.log.debug(`fetchAllMailsAsync: ${alias} - started`);
-      const folder = this.deps.folders.folders[alias];
-      mails[folder.folder] = await this.deps.mailbox.getMailAsync(folder.folder);
-      this.deps.log.debug(`fetchAllMailsAsync: ${alias} - fetched ${mails[folder.folder].length} emails`);
-      if (folder.screeningFolder !== folder.folder) {
-        this.deps.log.debug(`fetchAllMailsAsync: ${alias} - fetching screening folder (${folder.screeningFolder})`);
-        mails[folder.screeningFolder] = await this.deps.mailbox.getMailAsync(folder.screeningFolder);
-        this.deps.log.debug(`fetchAllMailsAsync: ${alias} - fetched ${mails[folder.screeningFolder].length} in screening folder`);
-      }
-      this.deps.log.debug(`fetchAllMailsAsync: ${alias} - done`);
-    }
-    return mails;
   }
 }
